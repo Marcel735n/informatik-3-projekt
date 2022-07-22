f8614a48d946310ff77993c36263e68e2f885449
Marcel Sobczak <m.sobczak@web.de>
2022-07-22 13:37:36 +0200

    Main Bildschrim

--- informatik-3-projekt/MyForm1.cpp
+++ informatik-3-projekt/MyForm1.cpp
@@ -1,3 +1,11 @@
 #include "MyForm1.h"
 
-#testtrs
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	informatik3projekt::MyForm1 form;
+	Application::Run(%form);
+}
--- informatik-3-projekt/MyForm1.h
+++ informatik-3-projekt/MyForm1.h
@@ -34,8 +34,14 @@ namespace informatik3projekt {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::CheckBox^  checkBox1;
-	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button5;
+	protected:
+
+
 	protected:
 
 	private:
@@ -51,41 +57,80 @@ namespace informatik3projekt {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm1::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
-			// checkBox1
+			// button1
 			// 
-			this->checkBox1->AutoSize = true;
-			this->checkBox1->Location = System::Drawing::Point(0, 0);
-			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(150, 29);
-			this->checkBox1->TabIndex = 0;
-			this->checkBox1->Text = L"checkBox1";
-			this->checkBox1->UseVisualStyleBackColor = true;
+			this->button1->Location = System::Drawing::Point(54, 117);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(117, 40);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm1::button1_Click);
 			// 
-			// comboBox1
+			// button2
 			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(49, 60);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(121, 33);
-			this->comboBox1->TabIndex = 1;
+			this->button2->Location = System::Drawing::Point(54, 170);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(117, 40);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(54, 216);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(117, 40);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(54, 271);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(117, 40);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"button4";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(54, 317);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(117, 40);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"button5";
+			this->button5->UseVisualStyleBackColor = true;
 			// 
 			// MyForm1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(274, 229);
-			this->Controls->Add(this->comboBox1);
-			this->Controls->Add(this->checkBox1);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(1220, 725);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::Fixed3D;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm1";
-			this->Text = L"MyForm1";
+			this->Text = L"Lagersystem";
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
 	};
 }
