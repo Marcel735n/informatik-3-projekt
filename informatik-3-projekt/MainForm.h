74005676d27981d8f9f431c72bb2024a8a918360
Marcel Sobczak <m.sobczak@web.de>
2022-08-02 10:22:13 +0200

    Update

--- informatik-3-projekt/MainForm.h
+++ informatik-3-projekt/MainForm.h
@@ -48,11 +48,21 @@ namespace informatik3projekt {
 	private: System::Windows::Forms::Button^  button3;
 	private: System::Windows::Forms::Button^  button4;
 	private: System::Windows::Forms::Button^  button5;
+
+
+
+
+
+	private: System::Windows::Forms::Panel^  panel4;
+
+	private: System::Windows::Forms::Button^  button7;
+	private: System::Windows::Forms::Button^  button8;
 	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
 	private: System::Windows::Forms::Button^  button6;
-	private: System::Windows::Forms::Label^  label6;
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
 
 
 
@@ -78,29 +88,34 @@ namespace informatik3projekt {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->button6 = (gcnew System::Windows::Forms::Button());
-			this->label6 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->panel2 = (gcnew System::Windows::Forms::Panel());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->panel4 = (gcnew System::Windows::Forms::Panel());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->panel3 = (gcnew System::Windows::Forms::Panel());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->panel1->SuspendLayout();
 			this->panel2->SuspendLayout();
 			this->panel3->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(29, 106);
+			this->button1->Location = System::Drawing::Point(12, 108);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(89, 23);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Eingabe";
 			this->button1->UseVisualStyleBackColor = true;
@@ -108,9 +123,9 @@ namespace informatik3projekt {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(29, 135);
+			this->button2->Location = System::Drawing::Point(12, 137);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->Size = System::Drawing::Size(89, 23);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Ausgabe";
 			this->button2->UseVisualStyleBackColor = true;
@@ -120,52 +135,13 @@ namespace informatik3projekt {
 			// 
 			this->panel1->BackColor = System::Drawing::SystemColors::Control;
 			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->panel1->Controls->Add(this->button6);
-			this->panel1->Controls->Add(this->label6);
-			this->panel1->Controls->Add(this->textBox1);
-			this->panel1->Controls->Add(this->label5);
 			this->panel1->Controls->Add(this->label2);
 			this->panel1->Enabled = false;
-			this->panel1->Location = System::Drawing::Point(202, 72);
+			this->panel1->Location = System::Drawing::Point(233, 100);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(343, 249);
+			this->panel1->Size = System::Drawing::Size(422, 307);
 			this->panel1->TabIndex = 2;
 			this->panel1->Visible = false;
-			
-			// 
-			// button6
-			// 
-			this->button6->Location = System::Drawing::Point(34, 87);
-			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(75, 23);
-			this->button6->TabIndex = 9;
-			this->button6->Text = L"Ausgabe:";
-			this->button6->UseVisualStyleBackColor = true;
-			this->button6->Click += gcnew System::EventHandler(this, &MainForm::button6_Click);
-			// 
-			// label6
-			// 
-			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(31, 45);
-			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(49, 13);
-			this->label6->TabIndex = 5;
-			this->label6->Text = L"Eingabe:";
-			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(34, 61);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 4;
-			// 
-			// label5
-			// 
-			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(31, 119);
-			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(0, 13);
-			this->label5->TabIndex = 3;
 			// 
 			// label2
 			// 
@@ -178,12 +154,16 @@ namespace informatik3projekt {
 			// 
 			// panel2
 			// 
+			this->panel2->AutoScroll = true;
 			this->panel2->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel2->Controls->Add(this->label5);
+			this->panel2->Controls->Add(this->button6);
+			this->panel2->Controls->Add(this->textBox1);
 			this->panel2->Controls->Add(this->label1);
 			this->panel2->Enabled = false;
-			this->panel2->Location = System::Drawing::Point(202, 72);
+			this->panel2->Location = System::Drawing::Point(233, 100);
 			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(343, 249);
+			this->panel2->Size = System::Drawing::Size(422, 307);
 			this->panel2->TabIndex = 3;
 			this->panel2->Visible = false;
 			// 
@@ -195,6 +175,15 @@ namespace informatik3projekt {
 			this->label1->Size = System::Drawing::Size(46, 13);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"Eingabe";
+			// 
+			// panel4
+			// 
+			this->panel4->AutoScroll = true;
+			this->panel4->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel4->Location = System::Drawing::Point(233, 100);
+			this->panel4->Name = L"panel4";
+			this->panel4->Size = System::Drawing::Size(422, 307);
+			this->panel4->TabIndex = 4;
 			// 
 			// label4
 			// 
@@ -210,17 +199,17 @@ namespace informatik3projekt {
 			this->panel3->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
 			this->panel3->Controls->Add(this->label4);
 			this->panel3->Enabled = false;
-			this->panel3->Location = System::Drawing::Point(202, 72);
+			this->panel3->Location = System::Drawing::Point(233, 100);
 			this->panel3->Name = L"panel3";
-			this->panel3->Size = System::Drawing::Size(343, 249);
+			this->panel3->Size = System::Drawing::Size(422, 307);
 			this->panel3->TabIndex = 4;
 			this->panel3->Visible = false;
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(29, 164);
+			this->button3->Location = System::Drawing::Point(12, 166);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->Size = System::Drawing::Size(89, 23);
 			this->button3->TabIndex = 5;
 			this->button3->Text = L"Suche";
 			this->button3->UseVisualStyleBackColor = true;
@@ -228,9 +217,9 @@ namespace informatik3projekt {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(29, 193);
+			this->button4->Location = System::Drawing::Point(12, 218);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->Size = System::Drawing::Size(89, 23);
 			this->button4->TabIndex = 6;
 			this->button4->Text = L"Menü";
 			this->button4->UseVisualStyleBackColor = true;
@@ -238,24 +227,85 @@ namespace informatik3projekt {
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(16, 222);
+			this->button5->Location = System::Drawing::Point(15, 336);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(108, 32);
+			this->button5->Size = System::Drawing::Size(89, 32);
 			this->button5->TabIndex = 7;
 			this->button5->Text = L"Beenden";
 			this->button5->UseVisualStyleBackColor = true;
 			this->button5->Click += gcnew System::EventHandler(this, &MainForm::button5_Click);
 			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(15, 307);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(89, 23);
+			this->button7->TabIndex = 8;
+			this->button7->Text = L"Hilfe";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(15, 278);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(89, 23);
+			this->button8->TabIndex = 9;
+			this->button8->Text = L"Einstellungen";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft JhengHei UI", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(5, 9);
+			this->label3->Location = System::Drawing::Point(12, 89);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(212, 41);
-			this->label3->TabIndex = 8;
-			this->label3->Text = L"Lagersystem";
+			this->label3->Size = System::Drawing::Size(42, 13);
+			this->label3->TabIndex = 10;
+			this->label3->Text = L"Bauteil:";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->pictureBox1->Location = System::Drawing::Point(491, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(136, 82);
+			this->pictureBox1->TabIndex = 11;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(6, 64);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(75, 23);
+			this->button6->TabIndex = 12;
+			this->button6->Text = L"Ausgabe:";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(6, 38);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 10;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(3, 98);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(0, 13);
+			this->label5->TabIndex = 13;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(12, 13);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(127, 24);
+			this->label6->TabIndex = 12;
+			this->label6->Text = L"Lagersystem";
 			// 
 			// MainForm
 			// 
@@ -263,27 +313,34 @@ namespace informatik3projekt {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::Control;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(557, 333);
+			this->ClientSize = System::Drawing::Size(667, 419);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
 			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->panel3);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->panel4);
 			this->DoubleBuffered = true;
+			this->HelpButton = true;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MainForm";
-			this->Text = L"MainForm";
+			this->Text = L"Lagersystem";
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
 			this->panel2->ResumeLayout(false);
 			this->panel2->PerformLayout();
 			this->panel3->ResumeLayout(false);
 			this->panel3->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -302,6 +359,9 @@ private: System::Void button1_Click(System::Object^  sender, System::EventArgs^ 
 	panel1->Enabled = false;
 	panel3->Visible = false;
 	panel3->Enabled = false;
+	panel4->Visible = false;
+	panel4->Enabled = false;
+
 }
 private: System::Void button2_Click_1(System::Object^  sender, System::EventArgs^  e) {
 	//Ausgabe
@@ -311,6 +371,8 @@ private: System::Void button2_Click_1(System::Object^  sender, System::EventArgs
 	panel2->Enabled = false;
 	panel3->Visible = false;
 	panel3->Enabled = false;
+	panel4->Visible = false;
+	panel4->Enabled = false;
 	
 }
 private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
@@ -321,6 +383,8 @@ private: System::Void button3_Click(System::Object^  sender, System::EventArgs^ 
 	panel1->Enabled = false;
 	panel3->Visible = true;
 	panel3->Enabled = true;
+	panel4->Visible = false;
+	panel4->Enabled = false;
 
 }
 private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
@@ -331,6 +395,8 @@ private: System::Void button4_Click(System::Object^  sender, System::EventArgs^ 
 	panel1->Enabled = false;
 	panel3->Visible = false;
 	panel3->Enabled = false;
+	panel4->Visible = true;
+	panel4->Enabled = true;
 }
 
 
@@ -345,6 +411,8 @@ private: System::Void button5_Click(System::Object^  sender, System::EventArgs^ 
 private: System::Void button6_Click(System::Object^  sender, System::EventArgs^  e) {
 	label5->Text = textBox1->Text;
 }
+
+
 
 };
 }
