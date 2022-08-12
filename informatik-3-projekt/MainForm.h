39f1f07931760f8b9bc3734768a430840895b4a0
Marcel Sobczak <m.sobczak@web.de>
2022-08-12 13:57:21 +0200

    Revert "Keine Veränderung"
    This reverts commit 4a13787460a11e36f56d611ed7848ec5e2109bb2.

--- informatik-3-projekt/MainForm.h
+++ informatik-3-projekt/MainForm.h
@@ -939,7 +939,7 @@ namespace informatik3projekt {
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Margin = System::Windows::Forms::Padding(2);
 			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
-			//this->Name = L"MainForm";
+			this->Name = L"MainForm";
 			this->Text = L"Lagersystem";
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
